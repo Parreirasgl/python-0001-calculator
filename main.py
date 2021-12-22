617bdc5c529a93e0eeb8fc78480333f297829619
Guilherme Parreiras <parreirasgl@hotmail.com>
2021-12-21 22:32:23 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+# Create a calculator.
+# Crie uma calculadora.
+
+while True:
+
+    print()
+    print("Options:")
+    print("Type 'add' to add:")
+    print("Type 'sub' to subtract:")
+    print("Type 'mul' to multiply:")
+    print("Type 'div' para divide:")
+    print("Type 'q' to quit:")
+
+    x = input()
+
+    if x == "q":
+        break
+
+    elif x == "add":
+        print()
+        num1 = float(input("Type a number: "))
+        num2 = float(input("Type another number: "))
+        result = str(num1 + num2)
+        print("The result is: " + result)
+
+    elif x == "sub":
+        print("sub")
+
+    elif x == "mul":
+        print("mult")
+
+    elif x == "div":
+        print("div")
+
+    else:
+        print("Unknown expression")
+        print("Try again")
+        continue
+
+
